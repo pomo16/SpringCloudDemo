1d40024e087f90a2b3818e3f8ddc2614506662f6
pomo <m13143378602@163.com>
2019-06-24 22:43:29 +0800

    Add Feign.

--- microservicecloud-consumer-dept-feign/src/main/java/com/pomo/springcloud/DeptConsumer80_Feign_App.java
+++ microservicecloud-consumer-dept-feign/src/main/java/com/pomo/springcloud/DeptConsumer80_Feign_App.java
@@ -3,9 +3,13 @@ package com.pomo.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableFeignClients(basePackages = {"com.pomo.springcloud"})
+@ComponentScan("com.pomo.springcloud")
 public class DeptConsumer80_Feign_App {
 	
 	public static void main(String[] args) {
